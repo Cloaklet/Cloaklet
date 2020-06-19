f53547603c3920f3e21054ebf5c9b05bab1d4f78
Joker_ <joker@mynook.info>
2020-06-19 18:26:54 +0800

    Add FUSEAvailable detection

--- rif.go
+++ rif.go
@@ -6,7 +6,10 @@ package main
 #include "rif.h"
 */
 import "C"
-import "unsafe"
+import (
+	"os"
+	"unsafe"
+)
 
 // RevealInFinder calls an Objective-C function (C_RevealInFinder defined in rif.h)
 // to select given `path` in its parent directory in Finder.
@@ -14,4 +17,13 @@ func RevealInFinder(path string) {
 	cPath := C.CString(path)
 	defer C.free(unsafe.Pointer(cPath))
 	C.C_RevealInFinder(cPath)
+}
+
+// FUSEAvailable tells if FUSE for Mac (a.k.a. OSXFUSE) is installed
+func FUSEAvailable() bool {
+	const loadBin string = "/Library/Filesystems/osxfuse.fs/Contents/Resources/load_osxfuse"
+	if info, err := os.Stat(loadBin); err == nil {
+		return !info.IsDir()
+	}
+	return false
 }
