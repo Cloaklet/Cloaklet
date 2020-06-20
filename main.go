3b5ba383f0b254e3abe84e49cdae6cc427dc8e9f
Joker_ <joker@mynook.info>
2020-06-20 08:48:33 +0800

    Improve code format

--- main.go
+++ main.go
@@ -12,9 +12,8 @@ import (
 var logger zerolog.Logger
 
 func init() {
-	logWriter := zerolog.ConsoleWriter{Out: os.Stdout, TimeFormat:time.RFC3339}
+	logWriter := zerolog.ConsoleWriter{Out: os.Stdout, TimeFormat: time.RFC3339}
 	logger = log.Logger.Output(logWriter).With().Str("app", "Cloaklet").Logger()
-	zerolog.SetGlobalLevel(zerolog.InfoLevel)
 	RegisterQmlMousePos("MousePos", 1, 0, "MousePos")
 	RegisterQmlVaultManager("VaultManager", 1, 0, "VaultManager")
 }
